cde88906ec104ad3caf102d84e1e8ebad95d7dc6
EscapeFromJava <reske.artur@gmail.com>
2022-03-26 22:34:45 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collections;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        String fileInput = "INPUT.txt";
+        String fileOutput = "OUTPUT.txt";
+        String[] list = Files.lines(Paths.get(fileInput)).reduce("", String::concat).split(" ");
+        String result = String.valueOf(GetValue(list[0], list[1]));
+        Files.write(Paths.get(fileOutput), Collections.singleton(result));
+    }
+
+    public static int GetValue(String a, String b){
+        int n1 = Integer.parseInt(String.valueOf(a.charAt(a.length()-1)));
+        int n2 = Integer.parseInt(String.valueOf(b.charAt(b.length()-1)));
+        String result = String.valueOf(Math.round(Math.pow(n1,n2)));
+        return Integer.parseInt(String.valueOf(result.charAt(result.length()-1)));
+    }
+}
